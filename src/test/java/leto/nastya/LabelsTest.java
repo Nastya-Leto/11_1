2efa0c1f771fc8be27697a5e359cd67b6304c70b
Anastasiya <soidberg91@gmail.com>
2023-03-31 09:15:11 +0300

    fix LabelsTest

--- src/test/java/leto/nastya/LabelsTest.java
+++ src/test/java/leto/nastya/LabelsTest.java
@@ -10,7 +10,6 @@ public class LabelsTest {
     @Test
     @Feature("Issue в репозитории")
     @Story("Создание Issue")
-    @Owner("ZakharovaAnAn")
     @Severity(SeverityLevel.MINOR)
     @Link(value = "Testing", url = "https://github.com/Nastya-Leto")
     @DisplayName("Создание Issue для авторизванного пользователя")
